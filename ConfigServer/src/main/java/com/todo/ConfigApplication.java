96695b588eea6e2c97ce1120de3b3841d6970bf3
msowjanya <msowjanya2014@gmail.com>
2018-08-18 18:28:09 +0530

    config server

---
+++ ConfigServer/src/main/java/com/todo/ConfigApplication.java
@@ -0,0 +1,23 @@
+package com.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/*************************************************************************************************************
+ *
+ * purpose:Config Server Application
+ * 
+ * @author sowjanya467
+ * @version 1.0
+ * @since 12-08-18
+ *
+ **************************************************************************************************/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+}
